f3c2afb323acdaddce4516131c3ca24e221f0d6a
Xieme <Xieme@>
2025-12-25 15:19:49 +0800

    添加项目文件。

---
+++ HJB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HJB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
